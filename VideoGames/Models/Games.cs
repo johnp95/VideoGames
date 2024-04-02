ef2d8dd5d825b6e6d2f018c1c502f4e5a6fa2ad1
John Pennyman <johnpennyman95@gmail.com>
2024-04-02 11:46:49 -0500

    add Games Model

---
+++ VideoGames/Models/Games.cs
@@ -0,0 +1,17 @@
+﻿namespace VideoGames.Models
+{
+    public class Games
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Developer { get; set; }
+
+        public string? Publisher { get; set; }
+
+        public string? Platform { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+    }
+}
