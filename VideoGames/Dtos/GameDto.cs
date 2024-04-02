8492aaa8506ba7fd95cf93251055081496ed8fde
John Pennyman <johnpennyman95@gmail.com>
2024-04-02 17:06:12 -0500

    add gamedto

---
+++ VideoGames/Dtos/GameDto.cs
@@ -0,0 +1,15 @@
+﻿namespace VideoGames.Dtos
+{
+    public class GameDto
+    {
+        public string Title { get; set; } = string.Empty;
+
+        public string Developer { get; set; } = string.Empty;
+
+        public string Publisher { get; set; } = string.Empty;
+
+        public string Platform { get; set; } = string.Empty;
+
+        public DateTime ReleaseDate { get; set; }
+    }
+}
