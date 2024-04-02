963799fa1752efd47de8c13ce097d1cf60e517ba
John Pennyman <johnpennyman95@gmail.com>
2024-04-02 17:06:38 -0500

    create igamerepository interface

---
+++ VideoGames/Interfaces/IGameRepository.cs
@@ -0,0 +1,18 @@
+﻿using VideoGames.Dtos;
+using VideoGames.Helpers;
+using VideoGames.Models;
+
+namespace VideoGames.Interfaces
+{
+    public interface IGameRepository
+    {
+        Task<List<Game>> GetAllAsync(QueryObject query);
+
+        Task<Game?> GetByIdAsync(int id);
+
+        Task<Game?> DeleteAsync(int id);
+
+        Task<Game> CreateAsync(Game gameModel);
+    }
+
+}
