16b0a90b411c2b2cc03668ba9e9871089b74f2fa
John Pennyman <johnpennyman95@gmail.com>
2024-04-02 12:14:53 -0500

    add  ApplicationDBContext file

---
+++ VideoGames/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VideoGames.Models;
+
+namespace VideoGames.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions)
+        {
+
+        }
+            public DbSet<Game> Games { get; set; }
+    }
+}
