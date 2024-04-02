ea682b5711dd9496185254a1c8886433fd22d009
John Pennyman <johnpennyman95@gmail.com>
2024-04-02 17:07:13 -0500

    create mappers for gotodto and gamefromcreate

---
+++ VideoGames/Mappers/GameMappers.cs
@@ -0,0 +1,33 @@
+﻿using VideoGames.Dtos;
+using VideoGames.Models;
+
+namespace VideoGames.Mappers
+{
+    public static class GameMappers
+    {
+        public static GameDto ToGameDto(this Game gameModel)
+        {
+            return new GameDto
+            {
+                Title = gameModel.Title,
+                Developer = gameModel.Developer,
+                Publisher = gameModel.Publisher,
+                Platform = gameModel.Platform,
+                ReleaseDate = gameModel.ReleaseDate
+            };
+            
+        }
+        public static Game ToGameFromCreate(this CreateGameDto gameDto)
+        {
+            return new Game
+            {
+                Title = gameDto.Title,
+                Developer = gameDto.Developer,
+                Publisher = gameDto.Publisher,
+                Platform = gameDto.Platform,
+                ReleaseDate = gameDto.ReleaseDate
+            };
+            
+        }
+    }
+}
