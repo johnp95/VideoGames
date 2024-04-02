c8232c27cf56866b188aa6f56d7dc058865d4e0e
John Pennyman <johnpennyman95@gmail.com>
2024-04-02 12:16:35 -0500

    change model name from Games to Game

---
+++ VideoGames/Models/Game.cs
@@ -0,0 +1,17 @@
+﻿namespace VideoGames.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Developer { get; set; }
+
+        public string? Publisher { get; set; }
+
+        public string? Platform { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+    }
+}
