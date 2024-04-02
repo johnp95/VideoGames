7325d85630b3cd970f44af9f423644cdf82eec1c
John Pennyman <johnpennyman95@gmail.com>
2024-04-02 17:06:24 -0500

    create queryobject

---
+++ VideoGames/Helpers/QueryObject.cs
@@ -0,0 +1,14 @@
+﻿namespace VideoGames.Helpers
+{
+    public class QueryObject
+    {
+        public string? Title { get; set; } = string.Empty;
+        public string? Developer { get; set; }
+
+        public string? Publisher { get; set; }
+
+        public string? Platform { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+    }
+}
