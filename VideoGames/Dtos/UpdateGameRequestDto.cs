c51d888410cc72002de421843451105dd25c657e
John Pennyman <johnpennyman95@gmail.com>
2024-04-11 11:39:52 -0500

    create update dto

---
+++ VideoGames/Dtos/UpdateGameRequestDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoGames.Dtos
+{
+    public class UpdateGameRequestDto
+    {
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public string Developer { get; set; } = string.Empty;
+
+        [Required]
+        public string Publisher { get; set; } = string.Empty;
+
+        [Required]
+        public string Platform { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
