97a555c06df7070b38aa9d8a677b219cc3bd6427
John Pennyman <johnpennyman95@gmail.com>
2024-04-12 15:47:49 -0500

    import toastr

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,7 +3,8 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()],
+  providers: [provideRouter(routes), provideHttpClient(), provideToastr()],
 };
