86d9e756d745cab6100745922cd8d4ddeb957ddf
John Pennyman <johnpennyman95@gmail.com>
2024-04-12 15:50:17 -0500

    init release date

---
+++ client/src/app/release-date/release-date.component.ts
@@ -0,0 +1,40 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { GamesService } from '../services/games.service';
+import { GameDetail } from '../interfaces/game-detail';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-release-date',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './release-date.component.html',
+  styleUrl: './release-date.component.css',
+})
+export class ReleaseDateComponent implements OnInit {
+  private gameService = inject(GamesService);
+
+  games: GameDetail[] = [];
+
+  ngOnInit(): void {
+    this.loadGames();
+  }
+
+  loadGames() {
+    this.gameService.getGames().subscribe({
+      next: (res: any) => {
+        this.games = res;
+        // Sort games by releaseDate (assuming releaseDate is a valid Date string)
+        this.games.sort(
+          (a, b) =>
+            new Date(a.releaseDate).getTime() -
+            new Date(b.releaseDate).getTime()
+        );
+        console.log(res);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+}
