363c87ad60c022899215dfec9dd3fcd2555ec254
John Pennyman <johnpennyman95@gmail.com>
2024-04-11 23:44:14 -0500

    add path to games

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { VideoGamesComponent } from './video-games/video-games.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'games', component: VideoGamesComponent },
+];
