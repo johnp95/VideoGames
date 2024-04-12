002d790208ed391ec6e16a2b5733c54d28cfc71d
John Pennyman <johnpennyman95@gmail.com>
2024-04-12 15:48:13 -0500

    create routes for pagenotfound, detail, release date, and form

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,19 @@
 import { Routes } from '@angular/router';
 import { VideoGamesComponent } from './video-games/video-games.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { VideoGameDetailComponent } from './video-game-detail/video-game-detail.component';
+import { ReleaseDateComponent } from './release-date/release-date.component';
+import { VideoGameFormComponent } from './video-game-form/video-game-form.component';
 
 export const routes: Routes = [
+  { path: '', title: 'Home', component: VideoGamesComponent },
+  { path: 'games/form', title: 'New Game', component: VideoGameFormComponent },
   { path: 'games', title: 'Games', component: VideoGamesComponent },
+  { path: 'games/:id', title: 'Games', component: VideoGameDetailComponent },
+  {
+    path: 'release-date',
+    title: 'Sort By Release',
+    component: ReleaseDateComponent,
+  },
+  { path: '**', title: 'Page Not Found', component: PageNotFoundComponent },
 ];
