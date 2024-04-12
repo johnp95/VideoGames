e4b306329cfef79b229ea0d9d4fc4fa4b0b74efb
John Pennyman <johnpennyman95@gmail.com>
2024-04-12 15:53:09 -0500

    add imgUrl to Game Model

--- VideoGames/Models/Game.cs
+++ VideoGames/Models/Game.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
 
         public string Title { get; set; } = string.Empty;
+        public string ImgUrl { get; set; } = string.Empty;
 
         public string Developer { get; set; } = string.Empty;
 
