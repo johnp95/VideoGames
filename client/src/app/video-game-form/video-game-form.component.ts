0903c06a54da81b0ad14f6bb4732df55304754fd
John Pennyman <johnpennyman95@gmail.com>
2024-04-13 12:48:44 -0500

    implement form ts file

--- client/src/app/video-game-form/video-game-form.component.ts
+++ client/src/app/video-game-form/video-game-form.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { GamesService } from '../services/games.service';
 
 @Component({
   selector: 'app-video-game-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, JsonPipe, RouterLink],
   templateUrl: './video-game-form.component.html',
   styleUrl: './video-game-form.component.css',
 })
-export class VideoGameFormComponent {}
+export class VideoGameFormComponent implements OnInit {
+  form!: FormGroup;
+  gameService = inject(GamesService);
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit() {
+    console.log('Form data:', this.form.value); // Log the form data
+    this.gameService.createGame(this.form.value).subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      title: [],
+      imgUrl: [],
+      developer: [],
+      publisher: [],
+      platform: [],
+      releaseDate: [],
+    });
+  }
+}
