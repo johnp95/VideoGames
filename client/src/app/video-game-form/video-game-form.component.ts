e515d0074e746f8c1d69d7f49ed19cee4e55dff4
John Pennyman <johnpennyman95@gmail.com>
2024-04-13 16:12:56 -0500

    add edit or create logic

--- client/src/app/video-game-form/video-game-form.component.ts
+++ client/src/app/video-game-form/video-game-form.component.ts
@@ -1,8 +1,15 @@
 import { JsonPipe } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { GamesService } from '../services/games.service';
+import { Subscription } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-video-game-form',
@@ -11,31 +18,82 @@ import { GamesService } from '../services/games.service';
   templateUrl: './video-game-form.component.html',
   styleUrl: './video-game-form.component.css',
 })
-export class VideoGameFormComponent implements OnInit {
+export class VideoGameFormComponent implements OnInit, OnDestroy {
   form!: FormGroup;
+  gameformSubscription!: Subscription;
+  paramsSubscription!: Subscription;
   gameService = inject(GamesService);
-  constructor(private fb: FormBuilder) {}
+
+  isEdit = false;
+  id = 0;
+
+  constructor(
+    private fb: FormBuilder,
+    private activatedRouter: ActivatedRoute,
+    private router: Router,
+    private toasterService: ToastrService
+  ) {}
+
+  ngOnDestroy(): void {
+    if (this.gameformSubscription) {
+      this.gameformSubscription.unsubscribe();
+    }
+    if (this.paramsSubscription) {
+      this.paramsSubscription.unsubscribe();
+    }
+  }
 
   onSubmit() {
-    console.log('Form data:', this.form.value); // Log the form data
-    this.gameService.createGame(this.form.value).subscribe({
-      next: (response) => {
-        console.log(response);
-      },
-      error: (err) => {
-        console.log(err);
-      },
-    });
+    if (!this.isEdit) {
+      this.gameformSubscription = this.gameService
+        .createGame(this.form.value)
+        .subscribe({
+          next: (response) => {
+            console.log(response);
+            this.toasterService.success('Game Successfully Added');
+            this.router.navigateByUrl('/games');
+          },
+          error: (err) => {
+            console.log(err);
+          },
+        });
+    } else {
+      this.gameService.editGame(this.id, this.form.value).subscribe({
+        next: (value) => {
+          this.toasterService.success('Edited successfully');
+          this.router.navigateByUrl('/games');
+        },
+        error: (err) => {
+          this.toasterService.error('Unable to edit');
+        },
+      });
+    }
   }
 
   ngOnInit(): void {
+    this.paramsSubscription = this.activatedRouter.params.subscribe({
+      next: (res) => {
+        console.log(res['id']);
+        let id = res['id'];
+        this.id = id;
+        if (!id) return;
+        this.gameService.getGame(id).subscribe({
+          next: (res) => {
+            this.form.patchValue(res);
+            this.isEdit = true;
+          },
+          error: (err) => console.log(err),
+        });
+      },
+      error: (err) => console.log(err),
+    });
     this.form = this.fb.group({
-      title: [],
+      title: ['', Validators.required],
       imgUrl: [],
-      developer: [],
-      publisher: [],
-      platform: [],
-      releaseDate: [],
+      developer: ['', Validators.required],
+      publisher: ['', Validators.required],
+      platform: ['', Validators.required],
+      releaseDate: ['', Validators.required],
     });
   }
 }
