6979719670eeace5b13861a7c5062842c7fd179d
John Pennyman <johnpennyman95@gmail.com>
2024-04-13 16:11:34 -0500

    put url in environment variable

--- client/src/app/services/games.service.ts
+++ client/src/app/services/games.service.ts
@@ -2,22 +2,29 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { GameDetail } from '../interfaces/game-detail';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GamesService {
-  apiUrl = 'http://localhost:5127/api/Games';
+  apiUrl = environment.URL;
   constructor(private http: HttpClient) {}
 
   getGames(): Observable<GameDetail> {
     return this.http.get<GameDetail>(this.apiUrl);
   }
-  getGame(id: string): Observable<GameDetail> {
-    return this.http.get<GameDetail>(this.apiUrl + '/' + id);
+  getGame(id: number): Observable<GameDetail> {
+    return this.http.get<GameDetail>(`${this.apiUrl}/${id}`);
   }
 
   createGame(data: GameDetail): Observable<any> {
     return this.http.post(this.apiUrl, data);
+  }
+  deleteGame(id: number) {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+  editGame(id: number, data: GameDetail) {
+    return this.http.put(`${this.apiUrl}/${id}`, data);
   }
 }
