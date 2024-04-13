b42a1408b9a2153de04297555c150929ddcba6e9
John Pennyman <johnpennyman95@gmail.com>
2024-04-13 16:10:25 -0500

    add animations to stop error

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,7 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), provideToastr()],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideToastr(),
+    provideAnimations(),
+  ],
 };
