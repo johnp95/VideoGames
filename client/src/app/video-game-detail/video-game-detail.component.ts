7b0146fd4520afbfcf735dc060cfa2f216c5e282
John Pennyman <johnpennyman95@gmail.com>
2024-04-13 16:12:25 -0500

    implement toaster and add delete functionality

--- client/src/app/video-game-detail/video-game-detail.component.ts
+++ client/src/app/video-game-detail/video-game-detail.component.ts
@@ -1,8 +1,9 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { GamesService } from '../services/games.service';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { GameDetail } from '../interfaces/game-detail';
 import { CommonModule } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-video-game-detail',
@@ -13,18 +14,34 @@ import { CommonModule } from '@angular/common';
 })
 export class VideoGameDetailComponent implements OnInit {
   game!: GameDetail;
-
+  toasterService = inject(ToastrService);
   constructor(
     private route: ActivatedRoute,
-    private gameDetailService: GamesService
+    private gameService: GamesService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    if (id) {
-      this.gameDetailService.getGame(id).subscribe((game) => {
-        this.game = game;
-      });
-    }
+    this.route.params.subscribe({
+      next: (res) => {
+        console.log(res['id']);
+        this.gameService.getGame(res['id']).subscribe({
+          next: (res) => {
+            this.game = res;
+            console.log(res);
+          },
+          error: (err) => console.log(err),
+        });
+      },
+      error: (err) => console.log(err),
+    });
+  }
+  delete(id: number) {
+    this.gameService.deleteGame(id).subscribe({
+      next: (response) => {
+        this.toasterService.success('Successfully Deleted');
+        this.router.navigateByUrl('/games');
+      },
+    });
   }
 }
