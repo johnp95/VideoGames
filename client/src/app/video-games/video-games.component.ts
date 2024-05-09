a36a3bd7fc4b2e44d6a1d0e4d20d3286f6ddd681
John Pennyman <johnpennyman95@gmail.com>
2024-05-09 17:51:57 -0500

    add search

--- client/src/app/video-games/video-games.component.ts
+++ client/src/app/video-games/video-games.component.ts
@@ -1,21 +1,22 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, Input, input, OnInit } from '@angular/core';
 import { GamesService } from '../services/games.service';
 import { GameDetail } from '../interfaces/game-detail';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-video-games',
   standalone: true,
-  imports: [AsyncPipe, CommonModule, RouterLink],
+  imports: [FormsModule, AsyncPipe, CommonModule, RouterLink],
   templateUrl: './video-games.component.html',
   styleUrl: './video-games.component.css',
 })
 export class VideoGamesComponent implements OnInit {
   private gameService = inject(GamesService);
-
-  games: GameDetail[] = [];
+  searchTerm: string = '';
+  @Input() games: GameDetail[] = [];
   toasterService = inject(ToastrService);
 
   ngOnInit(): void {
@@ -32,5 +33,10 @@ export class VideoGamesComponent implements OnInit {
         console.log(err);
       },
     });
+  }
+  get filteredGames(): GameDetail[] {
+    return this.games.filter((game) =>
+      game.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
   }
 }
